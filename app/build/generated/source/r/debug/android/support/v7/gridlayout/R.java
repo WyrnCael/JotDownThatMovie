d50a7022588259d1c1dfe19dee90f0d74fa4ee2a
WyrnCael <juanjpri@ucm.es>
2017-03-02 23:32:04 +0100

    Languaje in trailer and updated android version

--- app/build/generated/source/r/debug/android/support/v7/gridlayout/R.java
+++ app/build/generated/source/r/debug/android/support/v7/gridlayout/R.java
@@ -11,11 +11,13 @@ public final class R {
 		public static final int alignmentMode = 0x7f010004;
 		public static final int columnCount = 0x7f010002;
 		public static final int columnOrderPreserved = 0x7f010006;
-		public static final int layout_column = 0x7f010009;
-		public static final int layout_columnSpan = 0x7f01000a;
-		public static final int layout_gravity = 0x7f01000b;
+		public static final int layout_column = 0x7f01000a;
+		public static final int layout_columnSpan = 0x7f01000b;
+		public static final int layout_columnWeight = 0x7f01000c;
+		public static final int layout_gravity = 0x7f01000d;
 		public static final int layout_row = 0x7f010007;
 		public static final int layout_rowSpan = 0x7f010008;
+		public static final int layout_rowWeight = 0x7f010009;
 		public static final int orientation = 0x7f010000;
 		public static final int rowCount = 0x7f010001;
 		public static final int rowOrderPreserved = 0x7f010005;
@@ -46,7 +48,7 @@ public final class R {
 	}
 	public static final class styleable {
 		public static final int[] GridLayout = { 0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003, 0x7f010004, 0x7f010005, 0x7f010006 };
-		public static final int[] GridLayout_Layout = { 0x010100f4, 0x010100f5, 0x010100f6, 0x010100f7, 0x010100f8, 0x010100f9, 0x010100fa, 0x7f010007, 0x7f010008, 0x7f010009, 0x7f01000a, 0x7f01000b };
+		public static final int[] GridLayout_Layout = { 0x010100f4, 0x010100f5, 0x010100f6, 0x010100f7, 0x010100f8, 0x010100f9, 0x010100fa, 0x7f010007, 0x7f010008, 0x7f010009, 0x7f01000a, 0x7f01000b, 0x7f01000c, 0x7f01000d };
 		public static final int GridLayout_Layout_android_layout_height = 1;
 		public static final int GridLayout_Layout_android_layout_margin = 2;
 		public static final int GridLayout_Layout_android_layout_marginBottom = 6;
@@ -54,11 +56,13 @@ public final class R {
 		public static final int GridLayout_Layout_android_layout_marginRight = 5;
 		public static final int GridLayout_Layout_android_layout_marginTop = 4;
 		public static final int GridLayout_Layout_android_layout_width = 0;
-		public static final int GridLayout_Layout_layout_column = 9;
-		public static final int GridLayout_Layout_layout_columnSpan = 10;
-		public static final int GridLayout_Layout_layout_gravity = 11;
+		public static final int GridLayout_Layout_layout_column = 10;
+		public static final int GridLayout_Layout_layout_columnSpan = 11;
+		public static final int GridLayout_Layout_layout_columnWeight = 12;
+		public static final int GridLayout_Layout_layout_gravity = 13;
 		public static final int GridLayout_Layout_layout_row = 7;
 		public static final int GridLayout_Layout_layout_rowSpan = 8;
+		public static final int GridLayout_Layout_layout_rowWeight = 9;
 		public static final int GridLayout_alignmentMode = 4;
 		public static final int GridLayout_columnCount = 2;
 		public static final int GridLayout_columnOrderPreserved = 6;
