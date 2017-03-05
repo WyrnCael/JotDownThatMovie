5646b57a13a655a9435b12d402d5f392d7170202
WyrnCael <juanjpri@ucm.es>
2017-03-05 16:47:32 +0100

    Package name changed and gradle updated

--- app/src/main/java/com/fedorvlasov/lazylist/MainActivity.java
+++ app/src/main/java/com/fedorvlasov/lazylist/MainActivity.java
@@ -1,6 +1,6 @@
 package com.fedorvlasov.lazylist;
 
-import WyrnLab.pureba1.R;
+import com.wyrnlab.jotdownthatmovie.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
