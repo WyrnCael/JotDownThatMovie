85dab9332df9dc10a3e4945fc4c59f81361c3c08
Juan José Prieto <jpriesc@gmail.com>
2019-06-09 19:05:06 +0200

    Swipe to delete

--- app/src/main/java/com/wyrnlab/jotdownthatmovie/Recyclerviews/MovieViewHolder.java
+++ app/src/main/java/com/wyrnlab/jotdownthatmovie/Recyclerviews/MovieViewHolder.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.fedorvlasov.lazylist.ImageLoader;
@@ -21,7 +26,7 @@ import com.wyrnlab.jotdownthatmovie.search.RowItem;
 
 import static com.wyrnlab.jotdownthatmovie.Activities.MainActivity.REQUEST_CODE_A;
 
-public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener,View.OnCreateContextMenuListener {
 
     ImageView imageView;
     TextView txtTitle;
@@ -29,7 +34,11 @@ public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnC
     ImageView icon;
     Context context;
     ImageLoader imageLoader;
+    Button undoButton;
     RowItem item;
+    RecyclerViewClickListener itemListener;
+    int position;
+    View itemView;
 
     public MovieViewHolder(Context context, View itemView) {
         super(itemView);
@@ -40,15 +49,21 @@ public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnC
         this.imageView = (ImageView) itemView.findViewById(R.id.icon);
         this.icon = (ImageView) itemView.findViewById(R.id.iconType);
         this.imageLoader=new ImageLoader(context);
+        undoButton = (Button) itemView.findViewById(R.id.undo_button);
 
-        itemView.setOnClickListener(this);
+
+
+        this.itemView = itemView;
+        itemView.setOnCreateContextMenuListener(this);
     }
 
-    public void setMovieRow(RowItem rowItem) {
+    public void setMovieRow(RowItem rowItem, int position, RecyclerViewClickListener itemListener) {
         this.item = rowItem;
         this.imageView.setTag(rowItem.getImageId());
         this.txtDesc.setText(rowItem.getDesc());
         this.txtTitle.setText(rowItem.getTitle());
+        this.itemListener = itemListener;
+        this.position = position;
 
         if(rowItem.getType() == null || rowItem.getType().equalsIgnoreCase("Movie")){
             this.icon.setImageResource(R.drawable.video_camera);
@@ -61,46 +76,29 @@ public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnC
         else {
             this.imageView.setImageBitmap(ImageHandler.getImage((byte[]) rowItem.getImageId()));
         }
+
+        itemView.setOnClickListener(this);
+        itemView.setOnLongClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(this.item.getSource() == General.DB_SOURCE){
-            sendIntentDB();
-        } else {
-            sendIntentNET();
-        }
-    }
-
-    private void sendIntentDB(){
-        AudiovisualInterface pelicula = (AudiovisualInterface) (item).getObject();
-
-        Intent intent;
-        if(pelicula.getTipo() == null || pelicula.getTipo().equalsIgnoreCase(General.MOVIE_TYPE)){
-            intent =  new Intent(context, InfoMovieDatabase.class);
-        } else {
-            intent =  new Intent(context, InfoTVShowDatabase.class);
-        }
-        intent.putExtra("Pelicula", pelicula);
-        ((Activity) context).startActivityForResult(intent, REQUEST_CODE_A);
-    }
-
-    private void sendIntentNET(){
-        AudiovisualInterface pelicula = (AudiovisualInterface) (item).getObject();
-
-        Intent intent;
-        if(pelicula.getTipo().equalsIgnoreCase(General.MOVIE_TYPE)) {
-            intent = new Intent(context, InfoMovieSearch.class);
-
-        } else {
-            intent = new Intent(context, InfoTVShowSearch.class);
-        }
-        intent.putExtra("Pelicula", pelicula);
-        intent.putExtra("Type", pelicula.getTipo());
-        ((Activity) context).startActivityForResult(intent, General.REQUEST_CODE_PELIBUSCADA);
+        itemListener.recyclerViewListClicked(v, getAdapterPosition());
     }
 
     public void clearCache(){
         imageLoader.clearCache();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        itemListener.recylerViewCreateContextMenu(menu, v, menuInfo, getAdapterPosition());
+    }
+
+
+    @Override
+    public boolean onLongClick(View v) {
+        itemListener.recyclerViewListLongClicked(v, getAdapterPosition());
+        return false;
     }
 }
