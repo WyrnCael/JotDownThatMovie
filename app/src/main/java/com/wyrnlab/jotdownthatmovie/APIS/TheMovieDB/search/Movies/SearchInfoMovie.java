1cee902d6a9c560c696438acfd58c054cc0157cd
WyrnCael <jpriesc@gmail.com>
2020-09-01 20:27:42 +0200

    Similar Movies Completed

--- app/src/main/java/com/wyrnlab/jotdownthatmovie/APIS/TheMovieDB/search/Movies/SearchInfoMovie.java
+++ app/src/main/java/com/wyrnlab/jotdownthatmovie/APIS/TheMovieDB/search/Movies/SearchInfoMovie.java
@@ -11,9 +11,11 @@ import com.google.gson.Gson;
 import com.wyrnlab.jotdownthatmovie.APIS.TheMovieDB.search.AsyncResponse;
 import com.wyrnlab.jotdownthatmovie.ExternalLibraries.json.JsonArray;
 import com.wyrnlab.jotdownthatmovie.ExternalLibraries.json.JsonObject;
+import com.wyrnlab.jotdownthatmovie.Model.AudiovisualInterface;
 import com.wyrnlab.jotdownthatmovie.Model.General;
 import com.wyrnlab.jotdownthatmovie.Model.JSONModels.Movies.ModelCredits;
 import com.wyrnlab.jotdownthatmovie.Model.JSONModels.Movies.ModelMovie;
+import com.wyrnlab.jotdownthatmovie.Model.JSONModels.Movies.ModelSearchMovie;
 import com.wyrnlab.jotdownthatmovie.Model.Pelicula;
 import com.wyrnlab.jotdownthatmovie.R;
 import com.wyrnlab.jotdownthatmovie.Utils.ImageHandler;
@@ -26,6 +28,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -70,6 +74,7 @@ public class SearchInfoMovie extends AsyncTask<String, Integer, Pelicula> {
             getSinopsisPelicula();
             getCreditsPelicula();
             getImage();
+            getSimilars();
         } catch (IOException e) {
 
         }
@@ -98,7 +103,7 @@ public class SearchInfoMovie extends AsyncTask<String, Integer, Pelicula> {
         ModelMovie movie = new Gson().fromJson(json, ModelMovie.class);
         pelicula.setDataFromJson(movie);
         if(pelicula.getImagePath() == null) {
-            getOtrosPosters();
+            getOtrosPosters(pelicula);
         }
     }
 
@@ -146,18 +151,44 @@ public class SearchInfoMovie extends AsyncTask<String, Integer, Pelicula> {
         }
     }
 
-    private void getOtrosPosters() throws IOException{
-        String url = General.URLPRINCIPAL + "/3/movie/" + pelicula.getId() + "/images?api_key=" + General.APIKEY;
+    private void getOtrosPosters(AudiovisualInterface movie) throws IOException{
+        String url = General.URLPRINCIPAL + "/3/movie/" + movie.getId() + "/images?api_key=" + General.APIKEY;
 
-        leerJSONOtrosPosters(MyUtils.getHttpRequest(url));
+        leerJSONOtrosPosters(movie, MyUtils.getHttpRequest(url));
     }
 
-    private void leerJSONOtrosPosters(String json) throws IOException{
+    private void leerJSONOtrosPosters(AudiovisualInterface movie, String json) throws IOException{
         JsonObject info = JsonObject.readFrom( json );
         JsonArray aux = info.get("posters").asArray();
         if(aux.size() > 0){
             JsonObject poster = aux.get(0).asObject();
-            pelicula.setImagePath(poster.get("file_path").asString());
+            movie.setImagePath(poster.get("file_path").asString());
         }
+    }
+
+    private void getSimilars() throws IOException{
+        String url = General.URLPRINCIPAL + "3/movie/" + this.pelicula.getId() + "/similar?api_key=" + General.APIKEY + "&language=" + SetTheLanguages.getLanguage(Locale.getDefault().getDisplayLanguage());
+
+        readJSONSimilars(MyUtils.getHttpRequest(url));
+    }
+
+    private void readJSONSimilars(String json) throws IOException{
+        Log.d("SIMILARS: " , json);
+        ModelSearchMovie results = new Gson().fromJson(json, ModelSearchMovie.class);
+
+        List<AudiovisualInterface> similars = new ArrayList<AudiovisualInterface>();
+
+        if(results.results.length > 0) {
+            for (ModelMovie model : results.results) {
+                Pelicula movie = new Pelicula();
+                movie.setDataFromJson(model);
+                if(pelicula.getImagePath() == null) {
+                    getOtrosPosters(movie);
+                }
+                similars.add(movie);
+            }
+        }
+
+        this.pelicula.setSimilars(similars);
     }
 }
---
+++ app/src/main/java/com/wyrnlab/jotdownthatmovie/View/Activities/SimilarMoviesModal.java
@@ -0,0 +1,172 @@
+package com.wyrnlab.jotdownthatmovie.View.Activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroupOverlay;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.wyrnlab.jotdownthatmovie.APIS.TheMovieDB.search.AsyncResponse;
+import com.wyrnlab.jotdownthatmovie.DAO.DAO;
+import com.wyrnlab.jotdownthatmovie.JavaClasses.SaveAudiovisual;
+import com.wyrnlab.jotdownthatmovie.Model.AudiovisualInterface;
+import com.wyrnlab.jotdownthatmovie.Model.General;
+import com.wyrnlab.jotdownthatmovie.Model.RowItem;
+import com.wyrnlab.jotdownthatmovie.R;
+import com.wyrnlab.jotdownthatmovie.Utils.MyUtils;
+import com.wyrnlab.jotdownthatmovie.View.Activities.ShowInfo.mostrarPelicula.InfoMovieSearch;
+import com.wyrnlab.jotdownthatmovie.View.Activities.ShowInfo.showTVShow.InfoTVShowSearch;
+import com.wyrnlab.jotdownthatmovie.View.Recyclerviews.AdapterCallback;
+import com.wyrnlab.jotdownthatmovie.View.Recyclerviews.RecyclerViewAdapter;
+import com.wyrnlab.jotdownthatmovie.View.Recyclerviews.RecyclerViewClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+public class SimilarMoviesModal implements AsyncResponse, AdapterCallback, RecyclerViewClickListener {
+
+    Activity activity;
+    public AudiovisualInterface pelicula;
+    public RecyclerView listView;
+    List<RowItem> rowItems;
+    List<AudiovisualInterface> results;
+    RecyclerViewAdapter adapter;
+    int longClickPosition;
+    Context context;
+
+    public SimilarMoviesModal(AudiovisualInterface pelicula, Context context, Activity activity){
+        this.pelicula = pelicula;
+        this.context = context;
+        this.activity = activity;
+    }
+
+    public void createView(){
+        results = pelicula.getSimilars();
+        Log.d("Pelicula", pelicula.getTitulo());
+        rowItems = new ArrayList<RowItem>();
+        for (AudiovisualInterface movie : results) {
+            rowItems.add(new RowItem(context, movie));
+        }
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.similar_list, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+
+        Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int widthS = size.x;
+        int heightS = size.y;
+
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow.setElevation(20);
+        popupWindow.setWidth(widthS-80);
+        popupWindow.setHeight(heightS-80);
+
+        Drawable dim = new ColorDrawable(Color.BLACK);
+        ViewGroup root = (ViewGroup) activity.getWindow().getDecorView().getRootView();
+        dim.setBounds(0, 0, root.getWidth(), root.getHeight());
+        dim.setAlpha((int) (255 * 0.75));
+
+        ViewGroupOverlay overlay = root.getOverlay();
+        overlay.add(dim);
+
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+
+        listView = (RecyclerView) popupView.findViewById(R.id.list);
+        adapter = new RecyclerViewAdapter(context, this,
+                R.layout.list_item, rowItems, SimilarMoviesModal.this);
+        listView.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
+        listView.setLayoutManager(linearLayoutManager);
+        activity.registerForContextMenu(listView);
+
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                activity.getWindow().getDecorView().getRootView().getOverlay().clear();
+            }
+        });
+    }
+
+    public void removeAndSaveItem(Intent data){
+        adapter.remove(data.getIntExtra("Position", 0));
+    }
+
+    @Override
+    public void swipeCallback(AudiovisualInterface item) {
+    }
+
+    @Override
+    public void removeCallback(AudiovisualInterface item) {
+        SaveAudiovisual.saveItem(context, this, item, "Movie");
+    }
+
+    @Override
+    public void undoCallback(AudiovisualInterface item) {
+
+    }
+
+    @Override
+    public void recyclerViewListClicked(View v, int position) {
+        AudiovisualInterface pelicula = (AudiovisualInterface) ((RowItem)rowItems.get(position)).getObject();
+
+        Intent intent;
+        if(pelicula.getTipo().equalsIgnoreCase(General.MOVIE_TYPE)) {
+            intent = new Intent(context, InfoMovieSearch.class);
+
+        } else {
+            intent = new Intent(context, InfoTVShowSearch.class);
+        }
+        intent.putExtra("Pelicula", pelicula);
+        intent.putExtra("Type", pelicula.getTipo());
+        intent.putExtra("Position", position);
+        activity.startActivityForResult(intent, General.REQUEST_CODE_PELIBUSCADA);
+    }
+
+    @Override
+    public void recyclerViewListLongClicked(View v, int position) {
+
+    }
+
+    @Override
+    public void recylerViewCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo, int position) {
+
+    }
+
+    @Override
+    public void processFinish(Object result) {
+        if(DAO.getInstance().insert(context, (AudiovisualInterface) result)){
+            MyUtils.showSnacknar(listView, ((AudiovisualInterface) result).getTitulo() + " " + context.getResources().getString(R.string.added));
+        } else {
+            MyUtils.showSnacknar(listView, ((AudiovisualInterface) result).getTitulo() + " " + context.getResources().getString(R.string.alreadySaved));
+        }
+    }
+}
+
